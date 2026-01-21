327514e6161d34fb4fd493b12b9e02f39ccd4808
Josh Holman <git@jolman.me>
2026-01-20 21:18:18 -0800

    add headers

--- api/index.go
+++ api/index.go
@@ -6,6 +6,8 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{
 		"message": "hi there!",
 	})
